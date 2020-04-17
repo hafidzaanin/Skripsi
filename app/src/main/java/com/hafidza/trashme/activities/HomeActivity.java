d6c44829f79dcb7cdeaed3c33441469f1f93c54d
hafidzaanin <hafidzaanind@gmail.com>
2020-04-17 21:10:23 +0700

    versi kesebelas

--- app/src/main/java/com/hafidza/trashme/activities/HomeActivity.java
+++ app/src/main/java/com/hafidza/trashme/activities/HomeActivity.java
@@ -1,18 +1,21 @@
 package com.hafidza.trashme.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.hafidza.trashme.fragments.BantuanFragment;
 import com.hafidza.trashme.fragments.HomeFragment;
-import com.hafidza.trashme.fragments.KameraFragment;
 import com.hafidza.trashme.fragments.PengajuanFragment;
 import com.hafidza.trashme.fragments.ProfilFragment;
 import com.hafidza.trashme.R;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
+import android.provider.MediaStore;
 import android.view.MenuItem;
+
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -36,10 +39,8 @@ public class HomeActivity extends AppCompatActivity {
                             .commit();
                     return true;
                 case R.id.pengaduan:
-                    fragment = new KameraFragment();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container_layout, fragment, fragment.getClass().getSimpleName())
-                            .commit();
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 0);
                     return true;
                 case R.id.navigation_bantuan:
                     fragment = new BantuanFragment();
@@ -71,4 +72,8 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        }
 }
--- app/src/main/java/com/hafidza/trashme/fragments/PengaduanFragment.java
+++ app/src/main/java/com/hafidza/trashme/fragments/PengaduanFragment.java
@@ -7,35 +7,22 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.hafidza.trashme.R;
 
-public class KameraFragment extends Fragment {
+public class PengaduanFragment extends Fragment {
 
-    ImageView imageView;
+    private ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_kamera, container, false);
+        View view = inflater.inflate(R.layout.fragment_pengaduan, container, false);
 
-
-        Button btnCamera = (Button) view.findViewById(R.id.btnCamera);
         ImageView imageView = (ImageView) view.findViewById(R.id.imageCamera);
-
-
-        btnCamera.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick (View v){
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
-            }
-        });
         return view;
     }
 
